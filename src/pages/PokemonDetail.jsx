26c91abc8a05955830bef9f62679687d05cdc247
PierreLuc46 <luc.pierre46@gmail.com>
2026-02-03 12:22:36 +0100

    TP10 avec suppresion de pokemon dans la sidbar

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -12,20 +12,16 @@ function PokemonDetail() {
       .then((data) => setPokemon(data))
       .catch(err => console.error(err));
   }, [idPokemon]);
+
+
   if (!pokemon) return <div style={{color: 'white', textAlign: 'center'}}>Chargement...</div>;
+
   return (
     <div className="detail-container" style={{ textAlign: "center", padding: "50px" }}>
       <div style={{ marginBottom: "30px" }}>
-        <Link to="/" style={{ color: "#ffcc01", textDecoration: "none", fontSize: "1.2rem" }}>
-           Retour à l'accueil
-        </Link>
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <CardPokemon 
-            data={pokemon} 
-            pokedex={[]} 
-            setPokedex={() => {}} 
-        />
+        <CardPokemon data={pokemon} pokedex={[]} setPokedex={() => {}} />
       </div>
 
     </div>
